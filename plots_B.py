fbe5ca6eda0784e13a1f81980775fb63ef597091
Santiago <spenam@unal.edu.co>
2021-01-14 10:14:50 +0100

    Added some notes

--- plots_B.py
+++ plots_B.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.rcParams.update({'font.size':14})
+matplotlib.rcParams.update({'font.size':18})
 
 xang=np.genfromtxt("PMT_ang_acc.txt").transpose()[0]
 yang=np.genfromtxt("PMT_ang_acc.txt").transpose()[1]
@@ -12,7 +12,8 @@ yQE=np.genfromtxt("QEpmt.txt").transpose()[1]
 
 xwater=np.genfromtxt("water_len.txt").transpose()[0]
 ywater=np.genfromtxt("water_len.txt").transpose()[1]
-ywater2=np.genfromtxt("water_len.txt").transpose()[2]
+xwater2=np.genfromtxt("water_scatt_rayleight.txt").transpose()[0]
+ywater2=np.genfromtxt("water_scatt_rayleight.txt").transpose()[1]
 
 plt.plot(xang,yang)
 plt.grid(True)
@@ -33,12 +34,12 @@ plt.show()
 plt.plot(xwater,ywater)
 plt.grid(True)
 plt.xlabel(r"$\lambda$ [nm]")
-plt.ylabel(r"$\lambda_{abs}$ [nm]")
+plt.ylabel(r"$\lambda_{abs}$ [m]")
 plt.tight_layout()
 plt.savefig("water_abs.pdf")
 plt.show()
 
-plt.plot(xwater,ywater2)
+plt.plot(xwater2,ywater2)
 plt.grid(True)
 plt.xlabel(r"$\lambda$ [nm]")
 plt.ylabel(r"$\lambda_{sca}$ [m]")
